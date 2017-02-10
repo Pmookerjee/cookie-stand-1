cd256a6b9e11eb4bf3a783525406c1a60be275f5
Iryna Maslova <maslovai@hotmail.com>
2017-02-10 07:21:52 -0800

    firs store - all working

--- app.js
+++ app.js
@@ -14,3 +14,18 @@ var pikePlaceMarket = {
     }
   }
 }
+pikePlaceMarket.generateCookies();
+
+var parent = document.getElementById('Pike-Place');
+var child;
+var total = 0;
+for (i=0; i<8; i++){
+  child = document.createElement('li');
+  child.textContent= ( (10+i) + ' :' + ' ' + pikePlaceMarket.cookiesPerHour[i] + ' cookies');
+  total += pikePlaceMarket.cookiesPerHour[i];
+  parent.appendChild(child);
+  console.log(total);
+}
+child = document.createElement('li');
+child.textContent = ('Total cookies: ' + total);
+parent.appendChild(child);
