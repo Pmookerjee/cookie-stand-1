fbcb431dbc4b63c9e09f748aa81548b4bd084452
Iryna Maslova <maslovai@hotmail.com>
2017-02-09 20:37:01 -0800

    working java

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 var pikePlaceMarket = {
-  minCustomers: 17,
-  maxCustomers: 88,
+  min: 17,
+  max: 88,
   averageCookiesPerCustomer: 5.2,
-  customerPerHourFnctn: function (this.minCustomers, this.maxCustomers){
-  min = Math.ceil(this.minCustomers);
-  max = Math.floor(this.maxCustomers);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  customerPerHourFnctn: function(min, max){
+     //min = Math.ceil(this.minCustomers);
+     //max = Math.floor(this.maxCustomers);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  },
+  cookiesPerHour:[],
+  generateCookies:function(){
+    for (i=0; i<8; i++){
+      this.cookiesPerHour[i]=Math.round(this.customerPerHourFnctn(this.min, this.max)*this.averageCookiesPerCustomer);
+    }
   }
+}
