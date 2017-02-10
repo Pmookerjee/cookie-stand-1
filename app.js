bb99f142a03224b82e991fa6957c5d42398f36fb
Iryna Maslova <maslovai@hotmail.com>
2017-02-10 13:02:30 -0800

    adding south center

--- app.js
+++ app.js
@@ -59,8 +59,8 @@ var seaTac = {
 }
 //Call function to generate cookies amounts for SeaTac
 seaTac.generateCookies();
-// ul list for SeaTac:
 
+// ul list for SeaTac:
 parent = document.getElementById('seaTac');
 total = 0;
 for (i=0; i<8; i++){
@@ -77,5 +77,44 @@ for (i=0; i<8; i++){
 }
 // appending total for seaTac:
 child = document.createElement('li');
-child.textContent = ('Total:' + total + ' cookies');
+child.textContent = ('Total: ' + total + ' cookies');
+parent.appendChild(child);
+
+// generating object for Southcenter	11	38	1.9 store:
+var southCenter = {
+  min: 11,
+  max: 38,
+  averageCookiesPerCustomer: 1.9,
+  // random customer per hour number generator:
+  customerPerHourFnctn: function(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  },
+  cookiesPerHour:[],
+  // cookies per hour generator:
+  generateCookies:function(){
+    for (i=0; i<8; i++){
+      this.cookiesPerHour[i]=Math.round(this.customerPerHourFnctn(this.min, this.max)*this.averageCookiesPerCustomer);
+    }
+  }
+}//end of object
+
+southCenter.generateCookies();
+// ul for South Center
+total = 0;
+parent = document.getElementById('southCenter');
+for (i = 0; i<8; i++){
+  child = document.createElement('li');
+  if (i<2){
+    child.textContent= ( (10+i) + ' am:' + ' ' + southCenter.cookiesPerHour[i] + ' cookies');
+  } else  if (i===2){
+    child.textContent= ( '12 pm:' + ' ' + southCenter.cookiesPerHour[i] + ' cookies');
+  }else{
+    child.textContent= ( (i-2) + ' pm:' + ' ' + southCenter.cookiesPerHour[i] + ' cookies');
+  }
+  total += southCenter.cookiesPerHour[i];
+  parent.appendChild(child);
+}
+// Total li:
+child = document.createElement('li');
+child.textContent = 'Total : ' + total + ' cookies'
 parent.appendChild(child);
