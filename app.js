e97d1a44fc65c5970e35ac0872c247078939b5f2
Iryna Maslova <maslovai@hotmail.com>
2017-02-10 12:39:35 -0800

    added seaTac object

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
+// creating Pike Place Market stor object:
 var pikePlaceMarket = {
   min: 17,
   max: 88,
   averageCookiesPerCustomer: 5.2,
+  // random customer per hour number generator:
   customerPerHourFnctn: function(min, max){
-     //min = Math.ceil(this.minCustomers);
-     //max = Math.floor(this.maxCustomers);
     return Math.floor(Math.random() * (max - min + 1)) + min;
   },
   cookiesPerHour:[],
+  // cookies per hour generator:
   generateCookies:function(){
     for (i=0; i<8; i++){
       this.cookiesPerHour[i]=Math.round(this.customerPerHourFnctn(this.min, this.max)*this.averageCookiesPerCustomer);
     }
   }
 }
+//Call function to generate cookies amounts for Pike Place
 pikePlaceMarket.generateCookies();
-
+// creating <li> for Pike Place elements on page
 var parent = document.getElementById('Pike-Place');
 var child;
 var total = 0;
@@ -32,6 +34,48 @@ for (i=0; i<8; i++){
   parent.appendChild(child);
   console.log(total);
 }
+// appending total to the <ul>
 child = document.createElement('li');
 child.textContent = ('Total: ' + total + ' cookies');
+parent.appendChild(child);
+
+
+// creating SeaTac Airport	6	24	1.2 object:
+var seaTac = {
+  min: 6,
+  max: 24,
+  averageCookiesPerCustomer: 1.2,
+  // random customer per hour number generator:
+  customerPerHourFnctn: function(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  },
+  cookiesPerHour:[],
+  // cookies per hour generator:
+  generateCookies:function(){
+    for (i=0; i<8; i++){
+      this.cookiesPerHour[i]=Math.round(this.customerPerHourFnctn(this.min, this.max)*this.averageCookiesPerCustomer);
+    }
+  }
+}
+//Call function to generate cookies amounts for SeaTac
+seaTac.generateCookies();
+// ul list for SeaTac:
+
+parent = document.getElementById('seaTac');
+total = 0;
+for (i=0; i<8; i++){
+  child = document.createElement('li');
+  if (i<2){
+    child.textContent= ( (10+i) + ' am:' + ' ' + seaTac.cookiesPerHour[i] + ' cookies');
+  } else  if (i===2){
+    child.textContent= ( '12 pm:' + ' ' + seaTac.cookiesPerHour[i] + ' cookies');
+  }else{
+    child.textContent= ( (i-2) + ' pm:' + ' ' + seaTac.cookiesPerHour[i] + ' cookies');
+  }
+  total += seaTac.cookiesPerHour[i];
+  parent.appendChild(child);
+}
+// appending total for seaTac:
+child = document.createElement('li');
+child.textContent = ('Total:' + total + ' cookies');
 parent.appendChild(child);
